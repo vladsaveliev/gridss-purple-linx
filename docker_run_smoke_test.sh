532bd01f4b565b3e5e1212c0c3dbf9599b8662d1
Daniel Cameron <d-cameron@github.com>
2019-11-26 21:42:30 +1100

    Placing smoke testing script and data under source control

--- docker_run_smoke_test.sh
+++ docker_run_smoke_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker run --ulimit nofile=100000:100000 \
-	-v d:/refdata/hg19:/refdata \
-	-v d:/hartwig/smoke:/data/ \
+	-v s:/refdata/hg19:/refdata \
+	-v d:/dev/gridss-purple-linx/smoke_test:/data/ \
 	gridss/gridss-purple-linx:latest \
-	-n /data/CPCT12345678/CPCT12345678R/aligner/CPCT12345678R.bam \
-	-t /data/CPCT12345678/CPCT12345678T/aligner/CPCT12345678T.bam \
+	-n /data/CPCT12345678R.bam \
+	-t /data/CPCT12345678T.bam \
 	-v /data/smoketest.vcf \
 	-s smoketest \
 	--snvvcf /data/COLO829v003T.somatic_caller_post_processed.vcf.gz \
